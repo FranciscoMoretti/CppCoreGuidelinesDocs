7b189c9bd344c350c761cf2eee42050014a4de0c
Francisco Moretti <franciscoemoretti@gmail.com>
2022-07-19 11:48:41 +0100

    replaced missing content title

--- theme.config.js
+++ theme.config.js
@@ -24,7 +24,7 @@ export default {
       <meta name="twitter:url" content="https://nextra.vercel.app" />
       <meta name="og:title" content="C++ Core Guidelines" />
       <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="apple-mobile-web-app-title" content="C++ Core Guidelines" />
       <link
         rel="icon"
         type="image/png"
