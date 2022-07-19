78700d327da64ff26a0a8064c1fcf8298d079aaf
Francisco Moretti <franciscoemoretti@gmail.com>
2022-07-19 09:38:31 +0100

    replace footerText

--- theme.config.js
+++ theme.config.js
@@ -62,6 +62,6 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  footerText: <>C++ Core Guidelines {new Date().getFullYear()}.</>,
   unstable_faviconGlyph: '👋',
 }
